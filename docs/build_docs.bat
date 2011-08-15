1b69988068029b91539b681a0f826d362c1b3d3a
Christian S. Perone <christian.perone@gmail.com>
2011-08-15 18:40:32 -0300

    Changed dirs in the build_docs.bat from Python26 to Python27.

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -4,6 +4,6 @@ rd /s/q build_htmlhelp
 rd /s/q build_web
 rd /s/q build_latex
 
-c:\python26\scripts\sphinx-build -E -a -b htmlhelp .\source .\build_htmlhelp
-c:\python26\scripts\sphinx-build -E -a -b html .\source .\build_web
-c:\python26\scripts\sphinx-build -E -a -b latex .\source .\build_latex
+c:\python27\scripts\sphinx-build -E -a -b htmlhelp .\source .\build_htmlhelp
+c:\python27\scripts\sphinx-build -E -a -b html .\source .\build_web
+c:\python27\scripts\sphinx-build -E -a -b latex .\source .\build_latex
