09eeff77c76c36fa0d8e659f153fb8f39897797b
melevir <melevir@gmail.com>
2013-12-09 19:34:58 +0400

    More PEP8 fixes

--- pyevolve/G1DList.py
+++ pyevolve/G1DList.py
@@ -37,163 +37,164 @@ Class
 from GenomeBase import GenomeBase, G1DBase
 import Consts
 
+
 class G1DList(GenomeBase, G1DBase):
-   """ G1DList Class - The 1D List chromosome representation
-   
-   Inheritance diagram for :class:`G1DList.G1DList`:
+    """ G1DList Class - The 1D List chromosome representation
 
-   .. inheritance-diagram:: G1DList.G1DList
+    Inheritance diagram for :class:`G1DList.G1DList`:
 
-   This chromosome class extends the :class:`GenomeBase.GenomeBase`
-   and :class:`GenomeBase.G1DBase` classes.
-   
-   **Examples**
+    .. inheritance-diagram:: G1DList.G1DList
 
-      The instantiation
-         >>> g = G1DList(10)
+    This chromosome class extends the :class:`GenomeBase.GenomeBase`
+    and :class:`GenomeBase.G1DBase` classes.
 
-      Compare
-         >>> genome2 = genome1.clone()
-         >>> genome2 == genome1
-         True
+    **Examples**
 
-      Multiply
-         >>> genome = population[0]
-         >>> genome
-         (...)
-         [1, 2, 3, 4]
-         >>> genome_result = genome * 2
-         >>> genome_result
-         (...)
-         [2, 2, 6, 8]
+       The instantiation
+          >>> g = G1DList(10)
 
-      Add
-         >>> genome
-         (...)
-         [1, 2, 3, 4]
-         >>> genome_result = genome + 2
-         (...)
-         [3, 4, 5, 6]
-         
-      Iteration
-         >>> for i in genome:
-         >>>   print i
-         1
-         2
-         3
-         4
+       Compare
+          >>> genome2 = genome1.clone()
+          >>> genome2 == genome1
+          True
 
-      Size, slice, get/set, append
-         >>> len(genome)
-         4
-         >>> genome
-         (...)
-         [1, 2, 3, 4]
-         >>> genome[0:1]
-         [1, 2]
-         >>> genome[1] = 666
-         >>> genome
-         (...)
-         [1, 666, 3, 4]
-         >>> genome.append(99)
-         >>> genome
-         (...)
-         [1, 666, 3, 4, 99]
+       Multiply
+          >>> genome = population[0]
+          >>> genome
+          (...)
+          [1, 2, 3, 4]
+          >>> genome_result = genome * 2
+          >>> genome_result
+          (...)
+          [2, 2, 6, 8]
 
-   :param size: the 1D list size
+       Add
+          >>> genome
+          (...)
+          [1, 2, 3, 4]
+          >>> genome_result = genome + 2
+          (...)
+          [3, 4, 5, 6]
 
-   """
+       Iteration
+          >>> for i in genome:
+          >>>   print i
+          1
+          2
+          3
+          4
 
-   evaluator = None
-   """ This is the :term:`evaluation function` slot, you can add
-   a function with the *set* method: ::
+       Size, slice, get/set, append
+          >>> len(genome)
+          4
+          >>> genome
+          (...)
+          [1, 2, 3, 4]
+          >>> genome[0:1]
+          [1, 2]
+          >>> genome[1] = 666
+          >>> genome
+          (...)
+          [1, 666, 3, 4]
+          >>> genome.append(99)
+          >>> genome
+          (...)
+          [1, 666, 3, 4, 99]
 
-      genome.evaluator.set(eval_func)
-   """
+    :param size: the 1D list size
 
-   initializator = None
-   """ This is the initialization function of the genome, you
-   can change the default initializator using the function slot: ::
+    """
 
-      genome.initializator.set(Initializators.G1DListInitializatorAllele)
+    evaluator = None
+    """ This is the :term:`evaluation function` slot, you can add
+    a function with the *set* method: ::
 
-   In this example, the initializator :func:`Initializators.G1DListInitializatorAllele`
-   will be used to create the initial population.
-   """
+       genome.evaluator.set(eval_func)
+    """
 
-   mutator = None
-   """ This is the mutator function slot, you can change the default
-   mutator using the slot *set* function: ::
+    initializator = None
+    """ This is the initialization function of the genome, you
+    can change the default initializator using the function slot: ::
 
-      genome.mutator.set(Mutators.G1DListMutatorSwap)
+       genome.initializator.set(Initializators.G1DListInitializatorAllele)
 
-   """
+    In this example, the initializator :func:`Initializators.G1DListInitializatorAllele`
+    will be used to create the initial population.
+    """
 
-   crossover = None
-   """ This is the reproduction function slot, the crossover. You
-   can change the default crossover method using: ::
+    mutator = None
+    """ This is the mutator function slot, you can change the default
+    mutator using the slot *set* function: ::
 
-      genome.crossover.set(Crossovers.G1DListCrossoverUniform)
-   """
+       genome.mutator.set(Mutators.G1DListMutatorSwap)
 
-   def __init__(self, size=10, cloning=False):
-      """ The initializator of G1DList representation,
-      size parameter must be specified """
-      GenomeBase.__init__(self)
-      G1DBase.__init__(self, size)
-      if not cloning:
-         self.initializator.set(Consts.CDefG1DListInit)
-         self.mutator.set(Consts.CDefG1DListMutator)
-         self.crossover.set(Consts.CDefG1DListCrossover)
+    """
 
-   def __mul__(self, other):
-      """ Multiply every element of G1DList by "other" """
-      newObj = self.clone()
-      for i in xrange(len(newObj)):
-         newObj[i] *= other
-      return newObj
+    crossover = None
+    """ This is the reproduction function slot, the crossover. You
+    can change the default crossover method using: ::
 
-   def __add__(self, other):
-      """ Plus every element of G1DList by "other" """
-      newObj = self.clone()
-      for i in xrange(len(newObj)):
-         newObj[i] += other
-      return newObj
+       genome.crossover.set(Crossovers.G1DListCrossoverUniform)
+    """
 
-   def __sub__(self, other):
-      """ Plus every element of G1DList by "other" """
-      newObj = self.clone()
-      for i in xrange(len(newObj)):
-         newObj[i] -= other
-      return newObj
+    def __init__(self, size=10, cloning=False):
+        """ The initializator of G1DList representation,
+        size parameter must be specified """
+        GenomeBase.__init__(self)
+        G1DBase.__init__(self, size)
+        if not cloning:
+            self.initializator.set(Consts.CDefG1DListInit)
+            self.mutator.set(Consts.CDefG1DListMutator)
+            self.crossover.set(Consts.CDefG1DListCrossover)
 
-   def __repr__(self):
-      """ Return a string representation of Genome """
-      ret = GenomeBase.__repr__(self)
-      ret += "- G1DList\n"
-      ret += "\tList size:\t %s\n" % (self.getListSize(),)
-      ret += "\tList:\t\t %s\n\n" % (self.genomeList,)
-      return ret
+    def __mul__(self, other):
+        """ Multiply every element of G1DList by "other" """
+        newObj = self.clone()
+        for i in xrange(len(newObj)):
+            newObj[i] *= other
+        return newObj
 
-   def copy(self, g):
-      """ Copy genome to 'g'
-      
-      Example:
-         >>> genome_origin.copy(genome_destination)
-      
-      :param g: the destination G1DList instance
+    def __add__(self, other):
+        """ Plus every element of G1DList by "other" """
+        newObj = self.clone()
+        for i in xrange(len(newObj)):
+            newObj[i] += other
+        return newObj
 
-      """
-      GenomeBase.copy(self, g)
-      G1DBase.copy(self, g)
-   
-   def clone(self):
-      """ Return a new instace copy of the genome
-      
-      :rtype: the G1DList clone instance
+    def __sub__(self, other):
+        """ Plus every element of G1DList by "other" """
+        newObj = self.clone()
+        for i in xrange(len(newObj)):
+            newObj[i] -= other
+        return newObj
 
-      """
-      newcopy = G1DList(self.genomeSize, True)
-      self.copy(newcopy)
-      return newcopy
+    def __repr__(self):
+        """ Return a string representation of Genome """
+        ret = GenomeBase.__repr__(self)
+        ret += "- G1DList\n"
+        ret += "\tList size:\t %s\n" % (self.getListSize(),)
+        ret += "\tList:\t\t %s\n\n" % (self.genomeList,)
+        return ret
+
+    def copy(self, g):
+        """ Copy genome to 'g'
+
+        Example:
+           >>> genome_origin.copy(genome_destination)
+
+        :param g: the destination G1DList instance
+
+        """
+        GenomeBase.copy(self, g)
+        G1DBase.copy(self, g)
+
+    def clone(self):
+        """ Return a new instace copy of the genome
+
+        :rtype: the G1DList clone instance
+
+        """
+        newcopy = G1DList(self.genomeSize, True)
+        self.copy(newcopy)
+        return newcopy
 
