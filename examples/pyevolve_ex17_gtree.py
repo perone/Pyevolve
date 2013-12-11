f5d2c20ffbd31917e3cbef19969e1426054877f3
melevir <melevir@gmail.com>
2013-12-11 18:28:27 +0400

    Fixed examples indentaion 980 flake violations

--- examples/pyevolve_ex17_gtree.py
+++ examples/pyevolve_ex17_gtree.py
@@ -6,39 +6,39 @@ import time
 import random
 
 def eval_func(chromosome):
-   score = 0.0
-   # If you want to add score values based
-   # in the height of the Tree, the extra
-   # code is commented.
+    score = 0.0
+    # If you want to add score values based
+    # in the height of the Tree, the extra
+    # code is commented.
 
-   #height = chromosome.getHeight()
+    #height = chromosome.getHeight()
 
-   for node in chromosome:
-      score += (100 - node.getData())*0.1
+    for node in chromosome:
+        score += (100 - node.getData())*0.1
 
-   #if height <= chromosome.getParam("max_depth"):
-   #   score += (score*0.8)
+    #if height <= chromosome.getParam("max_depth"):
+    #   score += (score*0.8)
 
-   return score
+    return score
 
 def run_main():
-   genome = GTree.GTree()
-   root = GTree.GTreeNode(2)
-   genome.setRoot(root)
-   genome.processNodes()
+    genome = GTree.GTree()
+    root = GTree.GTreeNode(2)
+    genome.setRoot(root)
+    genome.processNodes()
 
-   genome.setParams(max_depth=3, max_siblings=2, method="grow")
-   genome.evaluator.set(eval_func)
-   genome.crossover.set(Crossovers.GTreeCrossoverSinglePointStrict)
+    genome.setParams(max_depth=3, max_siblings=2, method="grow")
+    genome.evaluator.set(eval_func)
+    genome.crossover.set(Crossovers.GTreeCrossoverSinglePointStrict)
 
-   ga = GSimpleGA.GSimpleGA(genome)
-   ga.setGenerations(100)
-   ga.setMutationRate(0.05)
-   
-   ga.evolve(freq_stats=10)
-   print ga.bestIndividual()
+    ga = GSimpleGA.GSimpleGA(genome)
+    ga.setGenerations(100)
+    ga.setMutationRate(0.05)
+
+    ga.evolve(freq_stats=10)
+    print ga.bestIndividual()
 
 if __name__ == "__main__":
-   run_main()
+    run_main()
 
   
--- examples/pyevolve_ex3_schaffer.py
+++ examples/pyevolve_ex3_schaffer.py
@@ -8,39 +8,39 @@ import math
 # large number of local minima, the global minimum is
 # at x=0,y=0 and there are many local minima around it
 def schafferF6(genome):
-   t1 = math.sin(math.sqrt(genome[0]**2 + genome[1]**2));
-   t2 = 1.0 + 0.001*(genome[0]**2 + genome[1]**2);
-   score = 0.5 + (t1*t1 - 0.5)/(t2*t2)
-   return score
+    t1 = math.sin(math.sqrt(genome[0]**2 + genome[1]**2));
+    t2 = 1.0 + 0.001*(genome[0]**2 + genome[1]**2);
+    score = 0.5 + (t1*t1 - 0.5)/(t2*t2)
+    return score
 
 def run_main():
-   # Genome instance
-   genome = G1DList.G1DList(2)
-   genome.setParams(rangemin=-100.0, rangemax=100.0, bestrawscore=0.0000, rounddecimal=4)
-   genome.initializator.set(Initializators.G1DListInitializatorReal)
-   genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
+    # Genome instance
+    genome = G1DList.G1DList(2)
+    genome.setParams(rangemin=-100.0, rangemax=100.0, bestrawscore=0.0000, rounddecimal=4)
+    genome.initializator.set(Initializators.G1DListInitializatorReal)
+    genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
 
-   # The evaluator function (objective function)
-   genome.evaluator.set(schafferF6)
+    # The evaluator function (objective function)
+    genome.evaluator.set(schafferF6)
 
-   # Genetic Algorithm Instance
-   ga = GSimpleGA.GSimpleGA(genome)
-   ga.selector.set(Selectors.GRouletteWheel)
+    # Genetic Algorithm Instance
+    ga = GSimpleGA.GSimpleGA(genome)
+    ga.selector.set(Selectors.GRouletteWheel)
 
-   ga.setMinimax(Consts.minimaxType["minimize"])
-   ga.setGenerations(8000)
-   ga.setMutationRate(0.05)
-   ga.setPopulationSize(100)
-   ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
+    ga.setMinimax(Consts.minimaxType["minimize"])
+    ga.setGenerations(8000)
+    ga.setMutationRate(0.05)
+    ga.setPopulationSize(100)
+    ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
 
-   # Do the evolution, with stats dump
-   # frequency of 10 generations
-   ga.evolve(freq_stats=250)
+    # Do the evolution, with stats dump
+    # frequency of 10 generations
+    ga.evolve(freq_stats=250)
 
-   # Best individual
-   best = ga.bestIndividual()
-   print best
-   print "Best individual score: %.2f" % best.getRawScore()
+    # Best individual
+    best = ga.bestIndividual()
+    print best
+    print "Best individual score: %.2f" % best.getRawScore()
 
 if __name__ == "__main__":
-   run_main()
+    run_main()
