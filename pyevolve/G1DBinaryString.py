572bebef4aa88da64e5295c8af20dd982f84de1e
melevir <melevir@gmail.com>
2013-12-19 06:25:56 +0400

    PEP8 fixes

--- pyevolve/G1DBinaryString.py
+++ pyevolve/G1DBinaryString.py
@@ -37,167 +37,167 @@ Class
 from GenomeBase import GenomeBase, G1DBase
 import Consts
 import Util
-    
+
+
 class G1DBinaryString(GenomeBase, G1DBase):
-   """ G1DBinaryString Class - The 1D Binary String chromosome
-   
-   Inheritance diagram for :class:`G1DBinaryString.G1DBinaryString`:
+    """ G1DBinaryString Class - The 1D Binary String chromosome
 
-   .. inheritance-diagram:: G1DBinaryString.G1DBinaryString
+    Inheritance diagram for :class:`G1DBinaryString.G1DBinaryString`:
 
-   This chromosome class extends the :class:`GenomeBase.GenomeBase`
-   and :class:`GenomeBase.G1DBase` classes.
-   
-   Example:
-      >>> genome = G1DBinaryString.G1DBinaryString(5)
+    .. inheritance-diagram:: G1DBinaryString.G1DBinaryString
 
-   :param length: the 1D Binary String size
+    This chromosome class extends the :class:`GenomeBase.GenomeBase`
+    and :class:`GenomeBase.G1DBase` classes.
 
-   """
+    Example:
+       >>> genome = G1DBinaryString.G1DBinaryString(5)
 
-   evaluator = None
-   """ This is the :term:`evaluation function` slot, you can add
-   a function with the *set* method: ::
+    :param length: the 1D Binary String size
 
-      genome.evaluator.set(eval_func)
-   """
+    """
 
-   initializator = None
-   """ This is the initialization function of the genome, you
-   can change the default initializator using the function slot: ::
+    evaluator = None
+    """ This is the :term:`evaluation function` slot, you can add
+    a function with the *set* method: ::
 
-      genome.initializator.set(Initializators.G1DBinaryStringInitializator)
+       genome.evaluator.set(eval_func)
+    """
 
-   In this example, the initializator :func:`Initializators.G1DBinaryStringInitializator`
-   will be used to create the initial population.
-   """
+    initializator = None
+    """ This is the initialization function of the genome, you
+    can change the default initializator using the function slot: ::
 
-   mutator = None
-   """ This is the mutator function slot, you can change the default
-   mutator using the slot *set* function: ::
+       genome.initializator.set(Initializators.G1DBinaryStringInitializator)
 
-      genome.mutator.set(Mutators.G1DBinaryStringMutatorSwap)
+    In this example, the initializator :func:`Initializators.G1DBinaryStringInitializator`
+    will be used to create the initial population.
+    """
 
-   """
+    mutator = None
+    """ This is the mutator function slot, you can change the default
+    mutator using the slot *set* function: ::
 
-   crossover = None
-   """ This is the reproduction function slot, the crossover. You
-   can change the default crossover method using: ::
+       genome.mutator.set(Mutators.G1DBinaryStringMutatorSwap)
 
-      genome.crossover.set(Crossovers.G1DBinaryStringXUniform)
-   """
+    """
 
+    crossover = None
+    """ This is the reproduction function slot, the crossover. You
+    can change the default crossover method using: ::
 
-   def __init__(self, length=10):
-      """ The initializator of G1DList representation """
-      GenomeBase.__init__(self)
-      G1DBase.__init__(self, length)
-      self.genomeList = []
-      self.stringLength = length
-      self.initializator.set(Consts.CDefG1DBinaryStringInit)
-      self.mutator.set(Consts.CDefG1DBinaryStringMutator)
-      self.crossover.set(Consts.CDefG1DBinaryStringCrossover)
+       genome.crossover.set(Crossovers.G1DBinaryStringXUniform)
+    """
 
-   def __setitem__(self, key, value):
-      """ Set the specified value for an gene of List
+    def __init__(self, length=10):
+        """ The initializator of G1DList representation """
+        GenomeBase.__init__(self)
+        G1DBase.__init__(self, length)
+        self.genomeList = []
+        self.stringLength = length
+        self.initializator.set(Consts.CDefG1DBinaryStringInit)
+        self.mutator.set(Consts.CDefG1DBinaryStringMutator)
+        self.crossover.set(Consts.CDefG1DBinaryStringCrossover)
 
-      >>> g = G1DBinaryString(5)
-      >>> for i in xrange(len(g)):
-      ...    g.append(1)
-      >>> g[4] = 0
-      >>> g[4]
-      0
+    def __setitem__(self, key, value):
+        """ Set the specified value for an gene of List
 
-      """
-      if value not in (0, 1):
-         Util.raiseException("The value must be zero (0) or one (1), used (%s)" % value, ValueError)
-      G1DBase.__setitem__(self, key, value)
+        >>> g = G1DBinaryString(5)
+        >>> for i in xrange(len(g)):
+        ...    g.append(1)
+        >>> g[4] = 0
+        >>> g[4]
+        0
 
-   def __repr__(self):
-      """ Return a string representation of Genome """
-      ret = GenomeBase.__repr__(self)
-      ret += "- G1DBinaryString\n"
-      ret += "\tString length:\t %s\n" % (self.getListSize(),)
-      ret += "\tString:\t\t %s\n\n" % (self.getBinary(),)
-      return ret
+        """
+        if value not in (0, 1):
+            Util.raiseException("The value must be zero (0) or one (1), used (%s)" % value, ValueError)
+        G1DBase.__setitem__(self, key, value)
 
-   def getDecimal(self):
-      """ Converts the binary string to decimal representation
+    def __repr__(self):
+        """ Return a string representation of Genome """
+        ret = GenomeBase.__repr__(self)
+        ret += "- G1DBinaryString\n"
+        ret += "\tString length:\t %s\n" % (self.getListSize(),)
+        ret += "\tString:\t\t %s\n\n" % (self.getBinary(),)
+        return ret
 
-      Example:
-         >>> g = G1DBinaryString(5)
-         >>> for i in xrange(len(g)):
-         ...    g.append(0)
-         >>> g[3] = 1
-         >>> g.getDecimal()
-         2
+    def getDecimal(self):
+        """ Converts the binary string to decimal representation
 
-      :rtype: decimal value
+        Example:
+           >>> g = G1DBinaryString(5)
+           >>> for i in xrange(len(g)):
+           ...    g.append(0)
+           >>> g[3] = 1
+           >>> g.getDecimal()
+           2
 
-      """
-      return int(self.getBinary(), 2)
+        :rtype: decimal value
 
-   def getBinary(self):
-      """ Returns the binary string representation
+        """
+        return int(self.getBinary(), 2)
 
-      Example:
-         >>> g = G1DBinaryString(2)
-         >>> g.append(0)
-         >>> g.append(1)
-         >>> g.getBinary()
-         '01'
+    def getBinary(self):
+        """ Returns the binary string representation
 
-      :rtype: the binary string
+        Example:
+           >>> g = G1DBinaryString(2)
+           >>> g.append(0)
+           >>> g.append(1)
+           >>> g.getBinary()
+           '01'
 
-      """
-      return "".join(map(str, self))
+        :rtype: the binary string
 
-   def append(self, value):
-      """ Appends an item to the list
+        """
+        return "".join(map(str, self))
 
-      Example:
-         >>> g = G1DBinaryString(2)
-         >>> g.append(0)
+    def append(self, value):
+        """ Appends an item to the list
 
-      :param value: value to be added, 0 or 1
+        Example:
+           >>> g = G1DBinaryString(2)
+           >>> g.append(0)
 
-      """
-      if value not in [0, 1]:
-         Util.raiseException("The value must be 0 or 1", ValueError)
-      G1DBase.append(self, value)
+        :param value: value to be added, 0 or 1
 
-   def copy(self, g):
-      """ Copy genome to 'g'
+        """
+        if value not in [0, 1]:
+            Util.raiseException("The value must be 0 or 1", ValueError)
+        G1DBase.append(self, value)
 
-      Example:
-         >>> g1 = G1DBinaryString(2)
-         >>> g1.append(0)
-         >>> g1.append(1)
-         >>> g2 = G1DBinaryString(2)
-         >>> g1.copy(g2)
-         >>> g2[1]
-         1
+    def copy(self, g):
+        """ Copy genome to 'g'
 
-      :param g: the destination genome
+        Example:
+           >>> g1 = G1DBinaryString(2)
+           >>> g1.append(0)
+           >>> g1.append(1)
+           >>> g2 = G1DBinaryString(2)
+           >>> g1.copy(g2)
+           >>> g2[1]
+           1
 
-      """
-      GenomeBase.copy(self, g)
-      G1DBase.copy(self, g)
-   
-   def clone(self):
-      """ Return a new instace copy of the genome
-      
-      Example:
-         >>> g = G1DBinaryString(5)
-         >>> for i in xrange(len(g)):
-         ...    g.append(1)
-         >>> clone = g.clone()
-         >>> clone[0]
-         1
+        :param g: the destination genome
 
-      :rtype: the G1DBinaryString instance clone
+        """
+        GenomeBase.copy(self, g)
+        G1DBase.copy(self, g)
 
-      """
-      newcopy = G1DBinaryString(self.getListSize())
-      self.copy(newcopy)
-      return newcopy
+    def clone(self):
+        """ Return a new instace copy of the genome
+
+        Example:
+           >>> g = G1DBinaryString(5)
+           >>> for i in xrange(len(g)):
+           ...    g.append(1)
+           >>> clone = g.clone()
+           >>> clone[0]
+           1
+
+        :rtype: the G1DBinaryString instance clone
+
+        """
+        newcopy = G1DBinaryString(self.getListSize())
+        self.copy(newcopy)
+        return newcopy
