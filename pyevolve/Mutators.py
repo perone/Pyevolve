10e91b46d3def696731160dbe4e6816abda6b00b
Ben Elliston <bje@air.net.au>
2014-09-19 21:27:05 +1000

    * Mutators.py (G1DListMutatorAlleleGaussian): New function. The existing   allele-based mutator is not particularly useful because   getRandomAllele() returns a new random allele value taken from the   uniform distribution (when real=True). A Gaussian mutator works better   in some cases.
      Note that only a single value range is supported.

--- pyevolve/Mutators.py
+++ pyevolve/Mutators.py
@@ -420,6 +420,52 @@ def G1DListMutatorAllele(genome, **args):
 
    return int(mutations)
 
+def G1DListMutatorAlleleGaussian(genome, **arguments):
+    """An allele-based mutator based on G1DListMutatorRealGaussian.
+
+    Accepts the parameter *gauss_mu* and the *gauss_sigma* which
+    respectively represents the mean and the std. dev. of the random
+    distribution.
+    """
+    if arguments["pmut"] <= 0.0:
+        return 0
+    listSize = len(genome)
+    mutations = arguments["pmut"] * listSize
+
+    mu = genome.getParam("gauss_mu")
+    sigma = genome.getParam("gauss_sigma")
+    if mu is None:
+        mu = Consts.CDefG1DListMutRealMU
+    if sigma is None:
+        sigma = Consts.CDefG1DListMutRealSIGMA
+
+    allele = genome.getParam("allele", None)
+    if allele is None:
+        Util.raiseException("to use this mutator, you must specify the 'allele' parameter", TypeError)
+
+    if mutations < 1.0:
+        mutations = 0
+        for it in xrange(listSize):
+            if Util.randomFlipCoin(arguments["pmut"]):
+                final_value = genome[it] + rand_gauss(mu, sigma)
+                assert len(allele[it].beginEnd) == 1, "only single ranges are supported"
+                rangemin, rangemax = allele[it].beginEnd[0]
+                final_value = min(final_value, rangemax)
+                final_value = max(final_value, rangemin)
+                genome[it] = final_value
+                mutations += 1
+    else:
+        for it in xrange(int(round(mutations))):
+            which_gene = rand_randint(0, listSize - 1)
+            final_value = genome[which_gene] + rand_gauss(mu, sigma)
+            assert len(allele[which_gene].beginEnd) == 1, "only single ranges are supported"
+            rangemin, rangemax = allele[which_gene].beginEnd[0]
+            final_value = min(final_value, rangemax)
+            final_value = max(final_value, rangemin)
+            genome[which_gene] = final_value
+    return int(mutations)
+
+
 ####################
 ##     2D List    ##
 ####################
