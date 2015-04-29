f9967b9185d42d815680ed57de448a7dfaf97202
gregor <gregor.olenik@web.de>
2015-04-29 19:45:03 +0200

    replace xrange by python3 range

--- pyevolve/GPopulation.py
+++ pyevolve/GPopulation.py
@@ -31,6 +31,7 @@ Class
 
 
 """
+from future.builtins import range
 
 import Consts
 import Util
@@ -106,7 +107,7 @@ class GPopulation(object):
          >>>   print ind
          (...)
 
-         >>> for i in xrange(len(pop)):
+         >>> for i in range(len(pop)):
          >>>    print pop[i]
          (...)
 
@@ -242,7 +243,7 @@ class GPopulation(object):
       raw_sum = 0
 
       len_pop = len(self)
-      for ind in xrange(len_pop):
+      for ind in range(len_pop):
          raw_sum += self[ind].score
 
       self.stats["rawMax"] = max(self, key=key_raw_score).score
@@ -250,7 +251,7 @@ class GPopulation(object):
       self.stats["rawAve"] = raw_sum / float(len_pop)
 
       tmpvar = 0.0
-      for ind in xrange(len_pop):
+      for ind in range(len_pop):
          s = self[ind].score - self.stats["rawAve"]
          s *= s
          tmpvar += s
@@ -353,11 +354,11 @@ class GPopulation(object):
    def create(self, **args):
       """ Clone the example genome to fill the population """
       self.minimax = args["minimax"]
-      self.internalPop = [self.oneSelfGenome.clone() for i in xrange(self.popSize)]
+      self.internalPop = [self.oneSelfGenome.clone() for i in range(self.popSize)]
       self.clearFlags()
 
    def __findIndividual(self, individual, end):
-      for i in xrange(end):
+      for i in range(end):
          if individual.compare(self.internalPop[i]) == 0:
             return True
 
@@ -367,7 +368,7 @@ class GPopulation(object):
       logging.debug("Initializing the population")
 
       if self.oneSelfGenome.getParam("full_diversity", True) and hasattr(self.oneSelfGenome, "compare"):
-         for i in xrange(len(self.internalPop)):
+         for i in range(len(self.internalPop)):
             curr = self.internalPop[i]
             curr.initialize(**args)
             while self.__findIndividual(curr, i):
@@ -393,7 +394,7 @@ class GPopulation(object):
             results = proc_pool.map(multiprocessing_eval_full, self.internalPop)
             proc_pool.close()
             proc_pool.join()
-            for i in xrange(len(self.internalPop)):
+            for i in range(len(self.internalPop)):
                self.internalPop[i] = results[i]
          else:
             results = proc_pool.map(multiprocessing_eval, self.internalPop)
@@ -417,7 +418,7 @@ class GPopulation(object):
          pass
 
       fit_sum = 0
-      for ind in xrange(len(self)):
+      for ind in range(len(self)):
          fit_sum += self[ind].fitness
 
       self.stats["fitMax"] = max(self, key=key_fitness_score).fitness
