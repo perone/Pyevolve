2ddfe1a1860c3a2caaf830d8b05045c8264c17df
BubaVV <markov.vadim@gmail.com>
2016-08-25 23:47:16 +0300

    Revert "Add files via upload"
    This reverts commit 07b000a81001ab6af707f0450fccefffe6137fce.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-#from distutils.core import setup #changed to distutils.core for pypy comptibility
-from setuptools import setup
+from distutils.core import setup #changed to distutils.core for pypy comptibility
 from pyevolve import __version__, __author__
-import sys
 
 setup(
    name = "Pyevolve",
@@ -11,11 +9,10 @@ setup(
    package_data = {
       'pyevolve': ['*.txt']
    },
-   test_suite = 'tests',
    author = __author__,
    author_email = "christian.perone@gmail.com",
    description = "A complete, free and open-source evolutionary framework written in Python",
    license = "PSF",
    keywords = "genetic algorithm genetic programming algorithms framework library python ai evolutionary framework",
-   url = "http://pyevolve.sourceforge.net/",
+   url = "http://pyevolve.sourceforge.net/"
 )
